6a621fcd41742a981f204a920b469a4bb42d8892
Wesley Monte <wesley.monte@ccc.ufcg.edu.br>
2020-07-06 10:41:34 -0300

    Updated docker helper script

---
+++ docker/docker.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+readonly REPOSITORY=ufcgsaps/common
+readonly USAGE="usage: docker.sh {build|push|publish} <TAG>"
+readonly MY_PATH=$(cd "$(dirname "${0}")" || { echo "For some reason, the path is not accessible"; exit 1; }; pwd )
+readonly WORKING_DIRECTORY="$(dirname "${MY_PATH}")"
+readonly DOCKER_FILE_PATH="${MY_PATH}/scheduler"
+
+build() {
+  local TAG="${1-latest}"
+  docker build --tag "${REPOSITORY}":"${TAG}" \
+            --file "${DOCKER_FILE_PATH}" "${WORKING_DIRECTORY}"
+}
+
+push() {
+  local TAG="${1-latest}"
+  docker push "${REPOSITORY}":"${TAG}"
+}
+
+main() {
+  if [ "$#" -eq 0 ]; then
+      echo "Use: ${USAGE}"
+      exit 1
+  fi
+  case ${1} in
+    build) shift
+      build "$@"
+      ;;
+    push) shift
+      push "$@"
+      ;;
+    publish) shift
+      build "$@"
+      push "$@"
+      ;;
+    *)
+      echo "${USAGE}"
+      exit 1
+  esac
+}
+
+main "$@"
